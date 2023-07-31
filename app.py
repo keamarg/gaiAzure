349b5e0d63ad38d6f3d12daf4bf46940c43dbaa2
Martin Gundtoft <marg@kea.dk>
2023-07-31 12:51:26 +0200

    databaseTest

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session
 from flask_cors import CORS
 from dotenv import load_dotenv
 import os
+import uuid
 
 import psycopg2  # for postgres
 import openai
@@ -12,6 +13,7 @@ import openai
 
 
 app = Flask(__name__)
+app.secret_key = "your_secret_key"  # Set a secret key for the session
 
 CORS(app)
 load_dotenv()  # Load environment variables from the .env file
@@ -20,6 +22,9 @@ openai.api_type = "azure"
 openai.api_base = "https://keaopenai.openai.azure.com/"
 openai.api_version = "2023-03-15-preview"  # subject to change
 openai.api_key = os.getenv("AZURE_API_KEY")
+
+# Configure the session interface
+app.config["SESSION_TYPE"] = "filesystem"
 
 # Get the PostgreSQL connection string from environment variable
 connection_string = os.getenv("AZURE_POSTGRESQL_CONNECTIONSTRING")
@@ -40,18 +45,80 @@ def test_database_connection():
         return False
 
 
-@app.route("/database")
+@app.route("/database", methods=["GET", "POST"])
 def database():
-    print("testing /database endpoint")
-    if test_database_connection():
-        return "Database connection successful"
-    else:
-        return "Unable to connect to the database"
+    print("this is the /database endpoint")
+
+    # Initialize the session ID if it doesn't exist
+    session_id = session.get("session_id")
+    if not session_id:
+        session_id = str(uuid.uuid4())  # Generate a new session ID
+        session["session_id"] = session_id  # Store the session ID in the session
+
+    if request.method == "GET":
+        try:
+            conn = psycopg2.connect(connection_string)
+            cursor = conn.cursor()
+
+            # Retrieve the entry based on the session ID
+            cursor.execute("SELECT content FROM chatdata WHERE id = %s", (session_id,))
+            entry = cursor.fetchone()
+
+            if entry:
+                # Return the content of the entry
+                return entry[0]
+            else:
+                cursor.close()
+                conn.close()
+                return "Entry not found"
+
+        except psycopg2.Error as e:
+            print(f"Error retrieving data from the database: {e}")
+            return "Unable to retrieve data"
+
+    elif request.method == "POST":
+        content = request.get_json().get("content")  # request.form.get("content")
+        if content:
+            try:
+                conn = psycopg2.connect(connection_string)
+                cursor = conn.cursor()
+
+                # Check if the session ID already exists in the database
+                cursor.execute(
+                    "SELECT * FROM chatdata WHERE session_id = %s", (session_id,)
+                )
+                existing_entry = cursor.fetchone()
+
+                if existing_entry:
+                    # Update the existing entry by appending the new content
+                    new_content = existing_entry[1] + content
+                    cursor.execute(
+                        "UPDATE chatdata SET content = %s WHERE session_id = %s",
+                        (new_content, session_id),
+                    )
+                else:
+                    # Insert a new entry with the session ID and content
+                    cursor.execute(
+                        "INSERT INTO chatdata (session_id, content) VALUES (%s, %s)",
+                        (session_id, content),
+                    )
+
+                conn.commit()
+                cursor.close()
+                conn.close()
+
+                return "Data saved to the database"
+
+            except psycopg2.Error as e:
+                print(f"Error saving data to the database: {e}")
+                return "Unable to save data"
+        else:
+            return "Invalid request: missing content"
 
 
 @app.route("/", methods=["POST"])
 def chat():
-    print("testing / endpoint")
+    print("this is the / endpoint")
     try:
         data = request.get_json()
         response = openai.ChatCompletion.create(
